93ebf4f1260d41fb85ae4832911be4ad5aa5c305
Mahmoud Galal <mahmoudegh8@gmail.com>
2025-12-31 19:35:36 +0200

    return to hash

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-// import { HashRouter } from "react-router-dom";
+// import { RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
-import { router } from "./router";
-// import { AppRoutes } from "./router";
+// import { router } from "./router";
+import { AppRoutes } from "./router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <RouterProvider router={router} />
-      {/* <HashRouter>
+      {/* <RouterProvider router={router} /> */}
+      <HashRouter>
         <AppRoutes />
-      </HashRouter> */}
+      </HashRouter>
     </GoogleOAuthProvider>
   </StrictMode>
 );
